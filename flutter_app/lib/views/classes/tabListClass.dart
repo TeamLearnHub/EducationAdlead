30ec734af3ca9be265f9bd62531bded190653c8e
datnt <datnt@fastgo.mobi>
2020-04-12 16:06:07 +0700

    Làm xong màn chi tiết khoá học - danh sách lớp

--- flutter_app/lib/views/classes/tabListClass.dart
+++ flutter_app/lib/views/classes/tabListClass.dart
@@ -4,8 +4,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/models/classes_model.dart';
+import 'package:flutter_app/views/home/all_classes_home.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:http/http.dart' as prefix0;
+
+import 'classes_detail.dart';
 
 class TabListClassPage extends StatefulWidget {
   @override
@@ -101,18 +104,205 @@ class TabListClassState extends State<TabListClassPage> {
     _listClasses = parsed
         .map<ClassesModel>((json) => ClassesModel.fromJson(json))
         .toList();
-    return _listClasses;
+    return _listClasses.sublist(0, 5);
   }
 
   Future<List<ClassesModel>> fetchClasses() async {
     final response =
-    await prefix0.get('http://demo4855049.mockable.io/gethotvideo');
+        await prefix0.get('http://demo4855049.mockable.io/gethotvideo');
     if (response.statusCode == 200) {
       return parseClasses(response.body);
     } else {
       throw Exception('Unable to fetch products from the REST API');
     }
   }
+
+  Widget allClassesWidget() {
+    return FutureBuilder(
+        future: fetchClasses(),
+        builder: (BuildContext context, AsyncSnapshot projectSnap) {
+          print(projectSnap.data);
+          if (projectSnap.connectionState == ConnectionState.none &&
+              projectSnap.hasData == null) {
+            return GestureDetector(
+              child: Center(
+                child: Image.network(
+                    "https://dzbbmecpa0hd2.cloudfront.net/video/avatar/2019/04/11/14/1554966002_de58c8a6be7d1046.jpg",
+                    height: 120,
+                    width: 120),
+              ),
+            );
+          }
+          return Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+                child: Align(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Các khoá học khác ',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.00),
+                    ),
+                    SizedBox(width: 105.0),
+                  ],
+                )),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                physics: ClampingScrollPhysics(),
+                itemCount: projectSnap?.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  ClassesModel project = projectSnap?.data[index];
+
+                  print(" -------" + project.avatar);
+                  return Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                        vertical: 8.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (context) {
+                                return ClassesDetailPage(classesModel: project);
+                              }));
+                            },
+                            child: Center(
+                              child: Image.network(project?.avatar,
+                                  height: 130, width: 130),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (context) {
+                                return ClassesDetailPage(classesModel: project);
+                              }));
+                            },
+                            onLongPress: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (context) {
+                                return ClassesDetailPage(classesModel: project);
+                              }));
+                            },
+                            child: Container(
+                              margin:
+                                  const EdgeInsets.only(top: 15.0, left: 12.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      SizedBox(width: 6.0),
+                                      Text('Kỹ năng quản lý thời gian',
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.bold))
+                                    ],
+                                  ),
+                                  SizedBox(height: 10.0),
+                                  Wrap(
+                                    crossAxisAlignment:
+                                        WrapCrossAlignment.center,
+                                    children: <Widget>[
+                                      Container(
+                                        child: new Image(
+                                            image: AssetImage(
+                                                'assets/ic_students.png'),
+                                            height: 18,
+                                            width: 25),
+                                      ),
+                                      Text('30M | ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.normal,
+                                              color: Hexcolor('#787878'))),
+                                      Container(
+                                        margin:
+                                            const EdgeInsets.only(bottom: 1.0),
+                                        child: new Image(
+                                            image: AssetImage(
+                                                'assets/ic_class.png'),
+                                            height: 18.0,
+                                            width: 25.0),
+                                      ),
+                                      Text('05 Lớp | ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.normal,
+                                              color: Hexcolor('#787878'))),
+                                      Container(
+                                        child: new Image(
+                                          image:
+                                              AssetImage('assets/ic_time.png'),
+                                          height: 18.0,
+                                          width: 25.0,
+                                        ),
+                                      ),
+                                      Text('07 ngày',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.normal)),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Container(
+                                    margin: const EdgeInsets.only(
+                                        left: 5.0,
+                                        top: 10.0,
+                                        bottom: 10.0,
+                                        right: 20.0),
+                                    child: InkWell(
+                                      onTap: () {},
+                                      child: new Container(
+                                        height: 35,
+                                        width:
+                                            MediaQuery.of(context).size.width /
+                                                3.2,
+                                        decoration: BoxDecoration(
+                                            gradient: LinearGradient(
+                                              colors: [
+                                                Hexcolor('#FAA244'),
+                                                Hexcolor('#FF8400')
+                                              ],
+                                            ),
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(50))),
+                                        child: Center(
+                                          child: Text(
+                                            'Đăng ký'.toUpperCase(),
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Image(
+                                      image: AssetImage('assets/ic_line.png')),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ));
+                },
+              )
+            ],
+          );
+        });
+  }
+
   Widget listClassWidget() {
     return FutureBuilder(
         future: fetchClasses(),
@@ -158,7 +348,7 @@ class TabListClassState extends State<TabListClassPage> {
                                 CircleAvatar(
                                   radius: 30.0,
                                   backgroundImage:
-                                  AssetImage('assets/logo_class.png'),
+                                      AssetImage('assets/logo_class.png'),
                                   backgroundColor: Colors.transparent,
                                 ),
                                 SizedBox(width: 20.0),
@@ -166,26 +356,32 @@ class TabListClassState extends State<TabListClassPage> {
                                   mainAxisAlignment: MainAxisAlignment.start,
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
-                                    Text('Kỹ năng quản lý thời gian',
-                                        style: TextStyle(
-                                            color: Hexcolor('#212121'),
-                                            fontSize: 16.0,
-                                            fontWeight: FontWeight.bold)),
+//                                    Text('Kỹ năng quản lý thời gian',
+//                                        style: TextStyle(
+//                                            color: Hexcolor('#212121'),
+//                                            fontSize: 16.0,
+//                                            fontWeight: FontWeight.bold)),
                                     SizedBox(height: 6.0),
                                     Row(
                                       children: <Widget>[
-                                        Text('Lớp K12  | ',
+                                        Text('Lớp 05  | ',
                                             style: TextStyle(
-                                                color: Hexcolor('#E8943A'),
+                                                color: Hexcolor('#212121'),
                                                 fontSize: 14.0,
                                                 fontWeight: FontWeight.normal)),
-                                        Text('Trạng thái : Active ',
+                                        Text('Mã lớp : 005 ',
                                             style: TextStyle(
-                                                color: Hexcolor('#E8943A'),
+                                                color: Hexcolor('#212121'),
                                                 fontSize: 16.0,
                                                 fontWeight: FontWeight.normal))
                                       ],
                                     ),
+                                    SizedBox(height: 5.0),
+                                    Text('đang chờ duyệt '.toUpperCase(),
+                                        style: TextStyle(
+                                            color: Hexcolor('#E8943A'),
+                                            fontSize: 12.0,
+                                            fontWeight: FontWeight.bold))
                                   ],
                                 )
                               ],
@@ -201,13 +397,29 @@ class TabListClassState extends State<TabListClassPage> {
                                   mainAxisAlignment: MainAxisAlignment.start,
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
-                                    Text('Giảng viên ',style: TextStyle(color: Hexcolor('#111111'),fontSize: 16.0,fontWeight: FontWeight.normal)),
+                                    Text('Loại hình ',
+                                        style: TextStyle(
+                                            color: Hexcolor('#111111'),
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.normal)),
                                     SizedBox(height: 8.0),
-                                    Text('Nguyễn Huy Anh ',style: TextStyle(color: Hexcolor('#111111'),fontSize: 16.0,fontWeight: FontWeight.bold)),
+                                    Text('Online ',
+                                        style: TextStyle(
+                                            color: Hexcolor('#111111'),
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.bold)),
                                     SizedBox(height: 13.0),
-                                    Text('Ngày bắt đầu',style: TextStyle(color: Hexcolor('#111111'),fontSize: 16.0,fontWeight: FontWeight.normal)),
+                                    Text('Số lượng',
+                                        style: TextStyle(
+                                            color: Hexcolor('#111111'),
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.normal)),
                                     SizedBox(height: 7.0),
-                                    Text('01/03/2020',style: TextStyle(color: Hexcolor('#111111'),fontSize: 16.0,fontWeight: FontWeight.bold)),
+                                    Text('12/30',
+                                        style: TextStyle(
+                                            color: Hexcolor('#111111'),
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.bold)),
                                   ],
                                 ),
                                 SizedBox(width: 40.0),
@@ -215,13 +427,29 @@ class TabListClassState extends State<TabListClassPage> {
                                   mainAxisAlignment: MainAxisAlignment.start,
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
-                                    Text('Chứng chỉ ',style: TextStyle(color: Hexcolor('#111111'),fontSize: 16.0,fontWeight: FontWeight.normal)),
+                                    Text('Giáo viên ',
+                                        style: TextStyle(
+                                            color: Hexcolor('#111111'),
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.normal)),
                                     SizedBox(height: 8.0),
-                                    Text('Chưa có',style: TextStyle(color: Hexcolor('#111111'),fontSize: 16.0,fontWeight: FontWeight.bold)),
+                                    Text('Lê Bảo Châu ',
+                                        style: TextStyle(
+                                            color: Hexcolor('#111111'),
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.bold)),
                                     SizedBox(height: 13.0),
-                                    Text('Ngày kết thúc',style: TextStyle(color: Hexcolor('#111111'),fontSize: 16.0,fontWeight: FontWeight.normal)),
+                                    Text('Thời gian ',
+                                        style: TextStyle(
+                                            color: Hexcolor('#111111'),
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.normal)),
                                     SizedBox(height: 7.0),
-                                    Text('03/03/2020',style: TextStyle(color: Hexcolor('#111111'),fontSize: 16.0,fontWeight: FontWeight.bold)),
+                                    Text('01/03/2020 - 20/03/2020',
+                                        style: TextStyle(
+                                            color: Hexcolor('#111111'),
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.bold)),
                                   ],
                                 )
                               ],
@@ -275,7 +503,11 @@ class TabListClassState extends State<TabListClassPage> {
                               print(value);
                             });
                           }))),
-
+              listClassWidget(),
+              SizedBox(height: 15.0),
+              Image(image: AssetImage('assets/ic_line_big.png')),
+              SizedBox(height: 15.0),
+              allClassesWidget()
             ],
           ),
         ));
